971f87920beda7a4ecbdc7cb728e770ff9ba43ef
MansiPathak <pathakmansi608@gmail.com>
2025-07-13 12:26:02 +0530

    error solved

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -374,7 +374,7 @@ import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 
 import "./shop.css";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL ="https://herbsfox.onrender.com"
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,23 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/',  // Change from empty string to root
-//   build: {
-//     outDir: 'dist',
-//     emptyOutDir: true
-//   }
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/products': 'http://localhost:5000',
-      '/admin': 'http://localhost:5000',
-    }
+  base: '/',  // Change from empty string to root
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/products': 'http://localhost:5000',
+//       '/admin': 'http://localhost:5000',
+//     }
+//   }
+// })
