29d1aca2c131e43773e204380414d697f701bae2
MansiPathak <pathakmansi608@gmail.com>
2025-07-15 09:09:58 +0530

    redundancy correction

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import ReactQuill from "react-quill";
-import ReactQuill from "react-quill"; 
+
 
 import "./AdminDashboard.css";
 
